d11cd20d71cd1a24a34c7b2fb9db017ea52a43e3
divaroni444 <divaroni444@gmail.com>
2025-12-27 02:40:41 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function openEnvelope() {
+  document.querySelector('.flap').style.transform = 'rotateX(180deg)';
+  document.querySelector('.letter').style.bottom = '0';
+  document.getElementById('music').play();
+}
